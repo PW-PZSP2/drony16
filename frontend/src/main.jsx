38dbf89164579d63732a448219bceae711b3d83d
koziolek24 <kozlowski11maciej@gmail.com>
2025-11-18 20:26:52 +0100

    minor fixes and linting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1>Frontend is working!</h1>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1>Frontend is working!</h1>,
+);
