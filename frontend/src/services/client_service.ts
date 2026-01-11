abf977f4c496a3e28b7b3d024564f1efd5d64d43
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2026-01-11 15:59:13 +0100

    implemented

--- frontend/src/services/client_service.ts
+++ frontend/src/services/client_service.ts
@@ -278,7 +278,16 @@ async function select_operator(
   orderId: number,
   operatorId: number,
 ): Promise<{ success: boolean; message?: string }> {
-  await mockDelay(API_DELAY);
+
+  const response = await fetch(`${API_URL}/orders/${orderId}/select/${operatorId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      credentials: "include",
+    },
+    credentials: "include",
+  });
+  console.log(response);
 
   if (!orderId || !operatorId) {
     return {
